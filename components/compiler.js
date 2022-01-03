220bd02f581f153ef574488b9cead27ea9db1dad
SinanGentoo <sinangentoo@gmail.com>
2022-01-03 12:08:25 +0800

    add lock

--- components/compiler.js
+++ components/compiler.js
@@ -1,12 +1,13 @@
 const process = require('child_process');
 const LocalStorage = require('node-localstorage').LocalStorage;
-const storage = new LocalStorage('./temp');
+const storage = new LocalStorage('./compiler');
 
 const config = require('../config.json');
 const logger = require('../util/logger.js');
 
 class Compiler {
     constructor (client) {
+        storage.setItem('lock', 'unlocked');
         this.client = client;
     }
 
@@ -15,13 +16,21 @@ class Compiler {
     }
     
     onGroupMessage (session) {
-        if (session.raw_message == '编译下预览站') {
-            if (this.isInList(session.user_id)) this.compile(session);
-            else session.reply('权限不足', true);
+        if (session.raw_message === '编译下预览站') {
+            if (this.isInList(session.user_id)) {
+                if (storage.getItem('lock') !== 'locked') {
+                    storage.setItem('lock', 'locked');
+                    this.compile(session);
+                } else session.reply("🔒已有一个进程正在运行")
+            } else session.reply('权限不足', true);
         }
         if (session.raw_message == '更新下预览站') {
-            if (this.isInList(session.user_id)) this.update(session);
-            else session.reply('权限不足', true);
+            if (this.isInList(session.user_id)) {
+                if (storage.getItem('lock') !== 'locked') {
+                    storage.setItem('lock', 'locked');
+                    this.update(session);
+                } else session.reply("🔒已有一个进程正在运行")
+            } else session.reply('权限不足', true);
         }
     }
     
@@ -36,6 +45,7 @@ class Compiler {
             process.exec('bash ' + config.rebuild_script, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
                 if (error !== null) session.reply('😞预览站编译失败了...\n请及时联系楠或Alex进行修复！', true);
                 else session.reply('🎉预览站编译成功啦\n预览站地址：https://codingclip.com/editor/dev/canary', true);
+                storage.setItem('lock', 'unlocked');
                 logger.debug(stdout);
             });
         } catch (e) {
@@ -49,6 +59,7 @@ class Compiler {
             process.exec('bash ' + config.update_script, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
                 if (error !== null) session.reply('😞预览站更新失败了...\n请及时联系楠或Alex进行修复！', true);
                 else session.reply('🎉预览站更新成功啦\n预览站地址：https://codingclip.com/editor/dev/canary', true);
+                storage.setItem('lock', 'unlocked');
                 logger.debug(stdout);
             });
         } catch (e) {
