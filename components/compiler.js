0f13e2e239fb0ba4bb365afb0b32adf1f7420c94
SinanGentoo <sinangentoo@gmail.com>
2022-01-03 11:31:58 +0800

    增加 更新预览站

--- components/compiler.js
+++ components/compiler.js
@@ -19,24 +19,40 @@ class Compiler {
             if (this.isInList(session.user_id)) this.compile(session);
             else session.reply('权限不足', true);
         }
+        if (session.raw_message == '更新下预览站') {
+            if (this.isInList(session.user_id)) this.update(session);
+            else session.reply('权限不足', true);
+        }
     }
     
     isInList (id) {
         const isAdmin = config.admin.includes(id);
-        const isUser = config.admin.includes(id);
-        return isAdmin || isUser;
+        return isAdmin;
     }
 
     async compile (session) {
-        session.reply('😄预览站更新中...', true);
+        session.reply('🔧预览站编译中...', true);
         try {
             process.exec('bash ' + config.rebuild_script, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
-                if (error !== null) session.reply('(• ▽ •;)预览站更新失败了...\n请及时联系Lauraceae或Alex进行修复！', true);
-                else session.reply('预览站更新成功啦( •̀ ω •́ )\n预览站地址：https://codingclip.com/editor/dev/canary', true);
+                if (error !== null) session.reply('😞预览站编译失败了...\n请及时联系楠或Alex进行修复！', true);
+                else session.reply('🎉预览站编译成功啦\n预览站地址：https://codingclip.com/editor/dev/canary', true);
                 logger.debug(stdout);
             });
         } catch (e) {
             logger.error('重编译期间出现错误：' + e);
+        }
+    }
+    
+    async update (session) {
+        session.reply('🔧预览站更新中...', true);
+        try {
+            process.exec('bash ' + config.update_script, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
+                if (error !== null) session.reply('😞预览站更新失败了...\n请及时联系楠或Alex进行修复！', true);
+                else session.reply('🎉预览站更新成功啦\n预览站地址：https://codingclip.com/editor/dev/canary', true);
+                logger.debug(stdout);
+            });
+        } catch (e) {
+            logger.error('更新期间出现错误：' + e);
         }
     }
 }
