03baa159fb7a8e4489526b45722da38e38ba114d
SimonShiki <sinangentoo@gmail.com>
2022-02-11 19:11:01 +0800

    诶嘿嘿 鸡汤来咯

--- components/sign.js
+++ components/sign.js
@@ -1,21 +1,49 @@
 const logger = require('../util/logger.js');
 const LocalStorage = require('node-localstorage').LocalStorage;
 const storage = new LocalStorage('./luck');
+const axios = require('axios');
 const dayjs = require('dayjs');
 
 class Sign {
     constructor (client) {
         this.client = client;
     }
-
+    
+    attempt (id) {
+        let list = storage.getItem('user');
+        if (!list) storage.setItem('user', '{}');
+        list = JSON.parse(list);
+        const attempt = list[id];
+        
+        if (!attempt) {
+            list[id] = 1;
+            storage.setItem('user', JSON.stringify(list));
+            return 1;
+        }
+        list[id] = attempt + 1;
+        storage.setItem('user', JSON.stringify(list));
+        return attempt + 1;
+    }
+    
     activate () {
         logger.info('签到组件加载成功！');
     }
     
-    onGroupMessage (session) {
+    async onGroupMessage (session) {
         if (session.raw_message === '签到') {
-            const jrrp = parseInt(session.user_id / this.getSeed()) % 101;
-            session.reply('签到成功(≧▽≦)！你今天的人品是：'+ jrrp);
+            const attempt = await this.attempt(parseInt(session.user_id));
+            if (attempt <= 1) {
+                const jrrp = parseInt(session.user_id / this.getSeed()) % 101;
+                session.reply('签到成功(≧▽≦)！你今天的人品是：'+ jrrp);
+            } else if (attempt == 2) {
+                session.reply('你知道吗，反复签到可是要掉脑袋的(๑•﹏•)');
+            } else {
+                try {
+                    this.client.setGroupBan(session.group_id, session.user_id, attempt * 120);
+                    const poisonous = await axios.get('https://api.muxiaoguo.cn/api/dujitang');
+                    session.reply(poisonous.data.data.comment);
+                } catch (e) {}
+            }
         }
     }
 
