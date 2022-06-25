9c7e4139ecb05358cf6971ea13af5b27d39b438c
SimonShiki <sinangentoo@gmail.com>
2022-06-25 13:07:26 +0800

    Abstract

--- components/execution.js
+++ components/execution.js
@@ -16,91 +16,33 @@ class Execution {
     }
     
     async onGroupMessage (session) {
-        this.handleCpp(session);
-        this.handleJs(session);
-        this.handlePy3(session);
-        this.handleGo(session);
-        this.handleLua(session);
-        this.handleBash(session);
+        this.handleRequest(session, 'cpp', '10.2.0');
+        this.handleRequest(session, 'javascript', '1.7.5');
+        this.handleRequest(session, 'python3', '3.1.10');
+        this.handleRequest(session, 'go', '1.16.2');
+        this.handleRequest(session, 'lua', '5.4.2');
+        this.handleRequest(session, 'bash', '5.1.0');
     }
     
-    async handleCpp (session) {
-        if (!session.raw_message.startsWith('!cpp ')) return;
+    async handleRequest (session, language, version) {
+        const command = `!${language} `;
+        if (!session.raw_message.startsWith(command)) return;
         if (!admin.includes(session.user_id)) return;
         if (!this.sandbox) session.reply('沙箱仍在初始化中');
+        
         session.reply('编译中...', true);
         const result = await this.sandbox.execute({
-            language: 'cpp',
-            version: '10.2.0'
-        }, session.raw_message.slice(5));
-        if (result.run.hasOwnProperty('stderr')) session.reply('运行失败:\n' + result.compile.stderr, true)
-        else session.reply('运行结果:\n' + result.run.stdout, true);
-    }
-    
-    async handleJs (session) {
-        if (!session.raw_message.startsWith('!js ')) return;
-        if (!admin.includes(session.user_id)) return;
-        if (!this.sandbox) session.reply('沙箱仍在初始化中');
-        session.reply('编译中...', true);
-        const result = await this.sandbox.execute({
-            language: 'javascript',
-            version: '1.7.5'
-        }, session.raw_message.slice(4));
-        if (result.run.hasOwnProperty('stderr')) session.reply('运行失败:\n' + result.run.stderr, true)
-        else session.reply('运行结果:\n' + result.run.stdout, true);
-    }
-    
-    async handlePy3 (session) {
-        if (!session.raw_message.startsWith('!py ')) return;
-        if (!admin.includes(session.user_id)) return;
-        if (!this.sandbox) session.reply('沙箱仍在初始化中');
-        session.reply('编译中...', true);
-        const result = await this.sandbox.execute({
-            language: 'python3',
-            version: '3.10.0'
-        }, session.raw_message.slice(4));
-        if (result.run.hasOwnProperty('stderr')) session.reply('运行失败:\n' + result.run.stderr, true)
-        else session.reply('运行结果:\n' + result.run.stdout, true);
-    }
-    
-    async handleGo (session) {
-        if (!session.raw_message.startsWith('!go ')) return;
-        if (!admin.includes(session.user_id)) return;
-        if (!this.sandbox) session.reply('沙箱仍在初始化中');
-        session.reply('编译中...', true);
-        const result = await this.sandbox.execute({
-            language: 'go',
-            version: '1.16.2'
-        }, session.raw_message.slice(4));
-        if (result.run.hasOwnProperty('stderr')) session.reply('运行失败:\n' + result.compile.stderr, true)
-        else session.reply('运行结果:\n' + result.run.stdout, true);
-    }
-    
-    async handleLua (session) {
-        if (!session.raw_message.startsWith('!lua ')) return;
-        if (!admin.includes(session.user_id)) return;
-        if (!this.sandbox) session.reply('沙箱仍在初始化中');
-        session.reply('编译中...', true);
-        const result = await this.sandbox.execute({
-            language: 'lua',
-            version: '5.4.2'
-        }, session.raw_message.slice(5));
-        if (result.run.hasOwnProperty('stderr')) session.reply('运行失败:\n' + result.run.stderr, true)
-        else session.reply('运行结果:\n' + result.run.stdout, true);
-    }
-    
-    async handleBash (session) {
-        if (!session.raw_message.startsWith('!bash ')) return;
-        if (!admin.includes(session.user_id)) return;
-        if (!this.sandbox) session.reply('沙箱仍在初始化中');
-        session.reply('编译中...', true);
-        const result = await this.sandbox.execute({
-            language: 'bash',
-            version: '5.1.0'
-        }, session.raw_message.slice(6));
-        if (result.run.hasOwnProperty('stderr')) session.reply('运行失败:\n' + result.run.stderr, true)
-        else session.reply('运行结果:\n' + result.run.stdout, true);
-    }
+            language: language,
+            version: version
+        }, session.raw_message.slice(command.length));
+        if (stderr in result.compile) {
+            let result = `编译失败:\n${result.compile.stderr}:`;
+            session.reply(result.trim());
+        }
+        let result = `STDOUT:\n${result.run.stdout}`;
+        if (stderr in result.run) result += '\nSTDERR:\n' + result.run.stderr;
+        else session.reply(result.trim(), true);
+    };
 }
 
 module.exports = Execution;
