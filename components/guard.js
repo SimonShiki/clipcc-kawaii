e43b67ca25b9aee12dc5355d9d082a8dd704aaaf
SimonShiki <sinangentoo@gmail.com>
2022-08-06 13:55:55 +0000

    substring

--- components/guard.js
+++ components/guard.js
@@ -5,8 +5,15 @@ class Yejun {
         this.client = client;
     }
     
+    isInList (msg) {
+        for (let taco of config.rbq) {
+            if (msg.indexOf(taco) !== -1) return true;
+        }
+        return false;
+    }
+    
     onGroupMessage (session) {
-        if (config.workgroup.includes(session.group_id) && config.rbq.includes(session.raw_message)) {
+        if (config.workgroup.includes(session.group_id) && this.isInList(session.raw_message)) {
             this.client.setGroupBan(session.group_id, session.user_id, 60);
         }
     }
