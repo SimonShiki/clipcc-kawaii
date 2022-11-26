1281b53ba95480603d359dddc16c800e3668adf1
SimonShiki <sinangentoo@gmail.com>
2022-11-26 15:14:17 +0000

    process
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- components/discord.js
+++ components/discord.js
@@ -10,16 +10,41 @@ class Discord {
         logger.info('Discord传话组件加载成功！');
     }
     async sendWebhook(session) {
+        const processedMessage = this.processMessage(session.message);
         await axios.post(config.discord_webhook,
           JSON.stringify({
-            content: session.raw_message,
-            username: session.sender.nickname
+            content: processedMessage,
+            username: session.sender.nickname,
+            avatar_url: session.member.getAvatarUrl(100)
           }),
         {
           headers: {
             'content-type': 'application/json'
           }
         })
+    }
+    processMessage (msgArray) {
+        const result = [];
+        for (const msgElem of msgArray) {
+            switch (msgElem.type) {
+            case 'face':
+            case 'at':
+            case 'text':
+                result.push(msgElem.text);
+                break;
+            case 'bface':
+                result.push(` ${msgElem.file} `);
+                break;
+            case 'share':
+            case 'flash':
+            case 'image':
+                result.push(` ${msgElem.url} `);
+                break;
+            default: 
+                result.push(`[${msgElem.type} msg]`);
+            }
+        }
+        return result.join('');
     }
     async onGroupMessage (session) {
         if (!config.workgroup.includes(session.group_id)) return;
