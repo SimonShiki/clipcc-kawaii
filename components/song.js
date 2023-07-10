9cf4819ff237f41db8f362ac65dfd534c61eaeb0
SimonShiki <sinangentoo@gmail.com>
2023-07-10 14:43:10 +0000

    song
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

---
+++ components/song.js
@@ -0,0 +1,40 @@
+const logger = require('../util/logger.js');
+const config = require('../config.json');
+const http = require("http");
+
+class Song {
+    constructor (client) {
+        this.client = client;
+    }
+
+    activate () {
+        logger.info('点歌组件加载成功！');
+    }
+    
+    async onGroupMessage (session) {
+        if (!config.workgroup.includes(session.group_id)) return;
+        
+        if (session.raw_message.startsWith('点歌')) {
+            const word = session.raw_message.replace("点歌", "").trim();
+            if (!word) return;
+            http.get(`http://s.music.163.com/search/get/?type=1&s=${word}&limit=1`, res => {
+                res.on("data", chunk => {
+                    try {
+                        const id = JSON.parse(String(chunk))?.result?.songs?.[0]?.id;
+                        if (id) {
+                            if (session.group?.shareMusic) session.group.shareMusic("163", id);
+                            if (session.friend?.shareMusic) session.friend.shareMusic("163", id);
+                        } else session.reply("未找到歌曲：" + word, true);
+                    } catch (e) {
+                        logger.error("请求歌曲API遇到错误：");
+                        logger.error(e);
+                    }
+                });
+            }).on("error", (e) => {
+                logger.error(e);
+            })
+        }
+    }
+}
+
+module.exports = Song;
