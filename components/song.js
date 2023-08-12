3ea71cc06e1b93bc79ea0457c92144e85a2273bd
SimonShiki <sinangentoo@gmail.com>
2023-08-12 13:40:16 +0000

    ajaj
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- components/song.js
+++ components/song.js
@@ -1,10 +1,41 @@
 const logger = require('../util/logger.js');
 const config = require('../config.json');
 const http = require("http");
+const schedule = require('node-schedule');
+const LocalStorage = require('node-localstorage').LocalStorage;
+const storage = new LocalStorage('./song');
 
 class Song {
     constructor (client) {
         this.client = client;
+        try {
+            this.songList = JSON.parse(storage.getItem('list'));
+        } catch (e) {
+            storage.setItem('list', '[]');
+        }
+        const job = schedule.scheduleJob({hour: 23, dayOfWeek: 0}, () => {
+            const group = this.client.pickGroup(config.ccgroup);
+            const word = this.songList[Math.floor(Math.random() * this.songList.length + 0.5)];
+            http.get(`http://s.music.163.com/search/get/?type=1&s=${word}&limit=1`, res => {
+                res.on("data", chunk => {
+                    try {
+                        const id = JSON.parse(String(chunk))?.result?.songs?.[0]?.id;
+                        if (id) {
+                            group.shareMusic("163", id);
+                        } else session.reply("未找到歌曲：" + word, true);
+                    } catch (e) {
+                        logger.error("请求歌曲API遇到错误：");
+                        logger.error(e);
+                    }
+                });
+            }).on("error", (e) => {
+                logger.error(e);
+            })
+        });
+        process.on('SIGINT', function () { 
+            schedule.gracefulShutdown()
+        .then(() => process.exit(0))
+}
     }
 
     activate () {
@@ -13,9 +44,9 @@ class Song {
     
     async onGroupMessage (session) {
         if (!config.workgroup.includes(session.group_id)) return;
-        
+        if (!admin.includes(session.user_id)) return;
+
         if (session.raw_message.startsWith('点歌')) {
-            if (!admin.includes(session.user_id)) return;
             const word = session.raw_message.replace("点歌", "").trim();
             if (!word) return;
             http.get(`http://s.music.163.com/search/get/?type=1&s=${word}&limit=1`, res => {
@@ -34,6 +65,12 @@ class Song {
             }).on("error", (e) => {
                 logger.error(e);
             })
+        } else if (session.raw_message.startsWith('加歌')) {
+            const word = session.raw_message.replace("点歌", "").trim();
+            if (!word) return;
+            this.songList.push(word);
+            storage.setItem('list', JSON.stringify(this.songList));
+            session.reply('加歌成功!', true);
         }
     }
 }
